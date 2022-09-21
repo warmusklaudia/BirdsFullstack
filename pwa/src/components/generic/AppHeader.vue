a1711c8c0f60cf11f98b46707b2ae4278bf49c63
Klaudia <klaudia.warmus@student.howest.be>
2022-09-21 16:40:15 +0200

    finish nav

--- pwa/src/components/generic/AppHeader.vue
+++ pwa/src/components/generic/AppHeader.vue
@@ -1,30 +1,29 @@
 <template>
-    <header class="px-6 py-6">
-        <nav class="max-w-6xl mx-auto flex items-center justify-between">
-            <router-link class="" to="">
-                <img src="/safari-pinned-tab.svg" class="w-12 pr-2 md:w-18" alt="">
-            </router-link>
-            <AppNavigation class="hidden sm:flex"/>
-        </nav>
-    </header>
+  <header class="px-6 py-6">
+    <nav class="mx-auto flex max-w-6xl items-center justify-between">
+      <router-link class="inline-block rounded-md outline-none focus-visible:ring-2" to="">
+        <img src="/safari-pinned-tab.svg" class="md:w-18 w-12 pr-2" alt="" />
+      </router-link>
+      <AppNavigation class="hidden sm:flex" />
+    </nav>
+  </header>
 </template>
 
 <script lang="ts">
-    import useAuthentication from '../../composables/useAuthentication';
-    import Logo from './Logo.vue'
-    import AppNavigation from '../generic/AppNavigation.vue'
+import useAuthentication from '../../composables/useAuthentication'
+import Logo from './Logo.vue'
+import AppNavigation from '../generic/AppNavigation.vue'
 
-    export default {
-
-        components: {
-            Logo,
-            AppNavigation
-        },
-        setup () {
-            const { user } = useAuthentication()
-            return {
-                user
-            }
-        }
+export default {
+  components: {
+    Logo,
+    AppNavigation,
+  },
+  setup() {
+    const { user } = useAuthentication()
+    return {
+      user,
     }
+  },
+}
 </script>
--- pwa/src/components/generic/AppNavigation.vue
+++ pwa/src/components/generic/AppNavigation.vue
@@ -1,30 +1,74 @@
 <template>
-                <ul class="grid w-full sm:w-auto grid-cols-4 items-center font-medium sm:space-x-3 text-center md:text-left text-sm">
-                <li><Home class="h-6 sm:hidden mb-2 mx-auto" /><router-link to="/" class=" md:text-lg lg:text-2xl font-medium">Home</router-link></li>
-                <li><Feather class="h-6 sm:hidden mb-2 mx-auto" /><router-link to="/birds" class=" md:text-lg lg:text-2xl font-medium">Vogels</router-link></li>
-                <li><Clipboard class="h-6 sm:hidden mb-2 mx-auto" /><router-link to="/log" class=" md:text-lg lg:text-2xl font-medium">Logboek</router-link></li>
-                <li><Scroll class="h-6 sm:hidden mb-2 mx-auto" /><router-link to="/observations" class=" md:text-lg lg:text-2xl font-medium">Observaties</router-link> </li>
-                <li class="hidden pl-6 sm:block">
-                    <router-link  to="/account" v-if="user" class="md:text-lg lg:text-2xl font-medium first-letter:uppercase">{{user?.displayName}}</router-link>
-                    <router-link to="/auth/login" v-else class="md:text-lg lg:text-2xl font-medium first-letter:uppercase">Login</router-link>
-                </li>
-            </ul>
+  <ul
+    class="grid w-full grid-cols-4 items-center text-center text-sm font-medium sm:w-auto sm:space-x-3 md:text-left"
+  >
+    <li>
+      <router-link
+        to="/"
+        exact-active-class="text-neutral-400"
+        class="inline-block rounded-md font-medium outline-none focus-visible:ring-2 md:text-lg lg:text-2xl"
+        ><Home class="mx-auto mb-2 h-6 sm:hidden" />Home</router-link
+      >
+    </li>
+    <li>
+      <router-link
+        to="/birds"
+        active-class="text-neutral-400"
+        class="inline-block rounded-md font-medium outline-none focus-visible:ring-2 md:text-lg lg:text-2xl"
+        ><Feather class="mx-auto mb-2 h-6 sm:hidden" />Vogels</router-link
+      >
+    </li>
+    <li>
+      <router-link
+        to="/log"
+        active-class="text-neutral-400"
+        class="inline-block rounded-md font-medium outline-none focus-visible:ring-2 md:text-lg lg:text-2xl"
+        ><Clipboard class="mx-auto mb-2 h-6 sm:hidden" />Logboek</router-link
+      >
+    </li>
+    <li>
+      <router-link
+        to="/observations"
+        active-class="text-neutral-400"
+        class="inline-block rounded-md font-medium outline-none focus-visible:ring-2 md:text-lg lg:text-2xl"
+        ><Scroll class="mx-auto mb-2 h-6 sm:hidden" />Observaties</router-link
+      >
+    </li>
+    <li class="hidden pl-6 sm:block">
+      <router-link
+        to="/account"
+        v-if="user"
+        active-class="text-neutral-400"
+        class="inline-block rounded-md font-medium outline-none first-letter:uppercase focus-visible:ring-2 md:text-lg lg:text-2xl"
+        >{{ user?.displayName }}</router-link
+      >
+      <router-link
+        to="/auth/login"
+        v-else
+        active-class="text-neutral-400"
+        class="inline-block rounded-md font-medium outline-none focus-visible:ring-2 md:text-lg lg:text-2xl"
+        >Login</router-link
+      >
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
-    import useAuthentication from '../../composables/useAuthentication';
-    import { Home, Feather, Scroll, Clipboard } from 'lucide-vue-next'
+import useAuthentication from '../../composables/useAuthentication'
+import { Home, Feather, Scroll, Clipboard } from 'lucide-vue-next'
 
-
-    export default {
-            components: {
-                Home, Feather, Scroll, Clipboard
-    },
-        setup () {
-            const { user } = useAuthentication()
-            return {
-                user
-            }
-        }
+export default {
+  components: {
+    Home,
+    Feather,
+    Scroll,
+    Clipboard,
+  },
+  setup() {
+    const { user } = useAuthentication()
+    return {
+      user,
     }
+  },
+}
 </script>
